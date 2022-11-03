3db7815ceb776ba2919ff39cb896f9cf4daebf6a
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2022-11-03 18:16:46 +0100

    feat: Add a list of all offers from a company in a page

--- src/Controller/ShowOfferController.php
+++ src/Controller/ShowOfferController.php
@@ -37,4 +37,37 @@ class ShowOfferController extends AbstractController
             'compatibilityPercentage' => $compatibilityPercentage,
         ]);
     }
+
+    #[Route('/show/company/{id}', name: 'app_show_company')]
+    public function showAllCompanyOffers(EntityManagerInterface $doctrine, int $id): Response
+    {
+        $company = $doctrine->getRepository(Companies::class)->find($id);
+        $offers = $doctrine->getRepository(Offers::class)->findBy(['company_id' => $id]);
+        $user = $doctrine->getRepository(Users::class)->findOneBy([], ['id' => 'DESC']);
+
+        // Place on the top the offers with more skills in common
+        usort($offers, function($a, $b) use ($user) {
+            $skillsA = $a->getSkills();
+            $skillsB = $b->getSkills();
+            $skillsCountA = 0;
+            $skillsCountB = 0;
+            foreach($skillsA as $skill) {
+                if($user->getSkills()->contains($skill)) {
+                    $skillsCountA++;
+                }
+            }
+            foreach($skillsB as $skill) {
+                if($user->getSkills()->contains($skill)) {
+                    $skillsCountB++;
+                }
+            }
+            return $skillsCountB <=> $skillsCountA;
+        });
+
+        return $this->render('show_offer/show_all_company_offers.html.twig', [
+            'company' => $company,
+            'offers' => $offers,
+            'user' => $user,
+        ]);
+    }
 }
