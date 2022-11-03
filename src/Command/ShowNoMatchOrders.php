4eef3b1d732180b33f900b97d84be168ee475d6b
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-11-03 09:49:23 +0100

    Create a command that will check if there is a match between users && offers and if there isn't any, print it

--- src/Command/ShowNoMatchOrders.php
+++ src/Command/ShowNoMatchOrders.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Command;
 
+use App\Entity\Companies;
 use App\Entity\Offers;
 
+use App\Entity\Users;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -21,23 +23,35 @@ class ShowNoMatchOrders extends Command
     // Execute
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("Expired offers :");
+        $output->writeln("Not matched offers :");
         $offers = $this->doctrine->getRepository(Offers::class)->findAll();
-        // Search offers by expiration date and if expired, display it
+        $users = $this->doctrine->getRepository(Users::class)->findAll();
         foreach ($offers as $offer) {
-            if ($offer->getEndDate() < new \DateTime()) {
+            $counter = 0;
+            // With offer.getCompanyId() get the company  that posted the offer
+            $company = $this->doctrine->getRepository(Companies::class)->find($offer->getCompanyId());
+            // Get zipcode of the company (split only 2 first numbers)
+            $companyZipcode = substr($company->getZipcode(), 0, 2);
+            foreach ($users as $user) {
+                $userZipcode = substr($user->getZipcode(), 0, 2);
+                if (!array_intersect($offer->getSkills(), $user->getSkills()) && $companyZipcode != $userZipcode) {
+                    $counter++;
+                }
+            }
+            if ($counter == count($users)) {
                 $output->writeln($offer->getName());
             }
         }
-        $output->writeln("End of expired offers");
+
+        $output->writeln("End of no match offers");
 
         return Command::SUCCESS;
     }
 
     protected function configure()
     {
-        $this->setName('app:show-expired-offers');
-        $this->setHelp('Show expired offers');
-        $this->setDescription('Show expired offers');
+        $this->setName('app:show-no-match-offers');
+        $this->setHelp('Show offers that does not match any profile');
+        $this->setDescription('Show offers that does not match any profile');
     }
 }
