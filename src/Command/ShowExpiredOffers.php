b7df5eb7fcb02da9cdf2a6e574e37d651a0a8e79
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-11-03 08:56:28 +0100

    Make command that will list all expired offers to our admin

--- src/Command/ShowExpiredOffers.php
+++ src/Command/ShowExpiredOffers.php
@@ -1,15 +1,36 @@
 <?php
 namespace App\Command;
 
-use Symfony\Component\Console\Attribute\AsCommand;
+use App\Entity\Offers;
+
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ShowExpiredOffers extends Command
 {
+
+    // Construct
+    public function __construct(EntityManagerInterface $doctrine)
+    {
+        $this->doctrine = $doctrine;
+        parent::__construct();
+    }
+
+    // Execute
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $output->writeln("Expired offers :");
+        $offers = $this->doctrine->getRepository(Offers::class)->findAll();
+        // Search offers by expiration date and if expired, display it
+        foreach ($offers as $offer) {
+            if ($offer->getEndDate() < new \DateTime()) {
+                $output->writeln($offer->getName());
+            }
+        }
+        $output->writeln("End of expired offers");
+
         return Command::SUCCESS;
     }
 
