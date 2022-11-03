62677f9ce7e335a786f6af7eed38ca2dc83bcd19
Thomas Lamiable <lamiablethomas@gmail.com>
2022-11-03 19:48:35 +0100

    🤌 - feature - Global hash system and login/register finish

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -23,9 +23,12 @@ class RegistrationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
-            $user->setPassword(
-                $form->get('plainPassword')->getData()
-            );
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $userPasswordHasher->hashPassword(
+            $user,
+            $form->get('plainPassword')->getData()
+        );
+        $user->setPassword($hashedPassword);
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
